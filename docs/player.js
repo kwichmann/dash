abf38d579eb2f83b29240e5cee30b8f35258e09e
kwichmann <kristian.wichmann@gmail.com>
2017-02-15 07:46:35 +0100

    Scrolling and basically animated player added

---
+++ docs/player.js
@@ -0,0 +1,112 @@
+function Player(x, y) {
+	this.x = x;
+	this.y = y;
+
+	this.blinking = false;
+	this.eyelids = 0;
+	this.eyelids_speed = 0;
+
+	this.move_left = false;
+	this.move_right = false;
+	this.move_up = false;
+	this.move_down = false;
+
+	this.feet = 0;
+	this.feet_speed = 1;
+
+	this.update = function() {
+		this.blink();
+		this.input();
+	}
+
+	this.blink = function() {		
+		if (this.blinking) {
+			this.eyelids += this.eyelids_speed;
+			if (this.eyelids <= 0) {
+				this.blinking = false;
+				this.eyelids = 0;
+				}
+			if (this.eyelids >= 12) {
+				this.eyelids_speed *= -1;
+				}
+			} else {
+				if (random() < 0.01) {
+					this.blinking = true;
+					this.eyelids_speed = 1.5;
+				}
+			}
+		}
+
+	this.input = function() {
+		this.move_left = false;
+		this.move_right = false;
+		this.move_up = false;
+		this.move_down = false;
+		
+		if (keyIsDown(LEFT_ARROW)) {
+			this.x -= 1;
+			this.move_left = true;
+		}
+		if (keyIsDown(RIGHT_ARROW)) {
+			this.x += 1;
+			this.move_right = true;
+		}
+		if (keyIsDown(UP_ARROW)) {
+			this.y -= 1;
+			this.move_up = true;
+		}
+		if (keyIsDown(DOWN_ARROW)) {
+			this.y += 1;
+			this.move_down = true;
+		}
+	}
+
+	this.draw = function() {
+		translate(this.x * 50, this.y * 50);
+
+		// Draw body
+		fill(0, 255, 0);
+		ellipse(25, 25, 30, 35);
+
+		var feet_yoffset = 0;
+		if (this.move_left || this.move_right || this.move_down || this.move_up) {
+			feet_yoffset = this.feet;
+			this.feet += this.feet_speed;
+			if (this.feet === 3 || this.feet === -3) {
+				this.feet_speed *= -1;
+			}
+		} else {
+			this.feet = 0;
+		}
+
+		// Draw feet
+		fill(0, 0, 255);
+		ellipse(10, 42 + feet_yoffset, 12, 10);
+		ellipse(40, 42 - feet_yoffset, 12, 10);
+
+		// Draw head
+		fill(255, 0, 0);
+		ellipse(25, 12, 45, 20);
+
+		var eye_xoffset = 0;
+		if (this.move_left) {
+			eye_xoffset -= 4;
+		}if (this.move_right) {
+			eye_xoffset += 4;
+		}
+
+		// Draw eyes
+		fill(255);
+		ellipse(20 + eye_xoffset, 10, 8, 12);
+		ellipse(30 + eye_xoffset, 10, 8, 12);
+
+		// Draw pupils
+		fill(0);
+		ellipse(20 + eye_xoffset, 10, 2, 4);
+		ellipse(30 + eye_xoffset, 10, 2, 4);
+
+		// Draw eyelids
+		fill(255, 0, 0);
+		rect(15 + eye_xoffset, 4, 20, this.eyelids);
+	}
+}
